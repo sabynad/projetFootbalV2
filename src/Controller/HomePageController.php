ed6b2e5f6f95ffc9a9e51c10b37447d41a79ac09
chafroud sabri <chafroud.sabri@hotmail.fr>
2024-04-16 17:30:27 +0200

    front footer retouche logo

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\OppositionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,11 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomePageController extends AbstractController
 {
     #[Route('/', name: 'app_home_page')]
-    public function index(): Response
+    public function index(OppositionRepository $oppositionRepository): Response
     {
         return $this->render('home_page/index.html.twig', [
             'controller_name' => 'HomePageController',
+            'opposition' => $oppositionRepository->findAll(),
         ]);
     }
   
