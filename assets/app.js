7347da0ac58ee5681a853bb3397e7bc2a12b6ccc
chafroud sabri <chafroud.sabri@hotmail.fr>
2024-05-07 12:19:37 +0200

    responsive ok

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,5 @@
 import './bootstrap.js';
+
 /*
  * Welcome to your app's main JavaScript file!
  *
@@ -9,7 +10,7 @@ import './styles/app.scss';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
 
-var ready = (callback) => {
+let ready = (callback) => {
     if (document.readyState != "loading") callback();
     else document.addEventListener("DOMContentLoaded", callback);
 }
@@ -22,4 +23,20 @@ ready(() => {
     document.querySelector('.burger-menu-close').addEventListener('click', () => {
         document.body.classList.remove('open');
     });
+    
+        // Sélectionnez le bouton de fermeture
+    const closeButton = document.querySelector('.burger-menu-close');
+
+    // Sélectionnez le menu burger
+    const burgerMenu = document.querySelector('.burger-menu-content');
+
+    // Ajoutez un gestionnaire d'événements au clic sur le bouton de fermeture
+    closeButton.addEventListener('click', function() {
+        // Masquez le menu burger en modifiant ses styles
+        burgerMenu.style.transform = 'translateX(-100%)';
+        burgerMenu.style.right = '100%';
+        // Ajoutez une classe au corps pour indiquer que le menu est fermé
+        document.body.classList.remove('open');
+    });
 });
+
